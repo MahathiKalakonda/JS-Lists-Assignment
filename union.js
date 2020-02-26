dea597b1efc5d6bde2e1ea0316caf47a13f38523
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-26 13:00:31 +0530

    created union.js file

---
+++ union.js
@@ -0,0 +1,12 @@
+function computeUnion(accumulator, list)
+{
+    return accumulator.concat(list);
+}
+
+function main()
+{
+    let firstBatch = [18, 20, 12, 13], secondBatch = [1, 19, 25, 32];
+    console.log("Union of people in batch 1 and batch 2 are " + computeUnion(firstBatch, secondBatch));
+}
+
+main()
