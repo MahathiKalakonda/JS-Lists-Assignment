bcddee1daba5f7c3c71a8f375b99701067b625a1
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-26 14:35:55 +0530

    removed main call

--- firstNElements.js
+++ firstNElements.js
@@ -8,5 +8,3 @@ function main()
     let number = 4, elements = [23, 12, 9, 13, 29, 91, 32];
     console.log("First " + number + " elements are : " + getFirstNElements(elements, number));
 }
-
-main();
