91f340240b413ab6a8607ee770417cbddb1cdc74
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-26 14:36:04 +0530

    removed main call

--- union.js
+++ union.js
@@ -8,5 +8,3 @@ function main()
     let firstBatch = [18, 20, 12, 13], secondBatch = [1, 19, 25, 32];
     console.log("Union of people in batch 1 and batch 2 are " + computeUnion(firstBatch, secondBatch));
 }
-
-main()
