f8df6e427f2c012bb52f8cb6d1da7ef7638c9481
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-26 14:34:14 +0530

    created cycleList file

---
+++ cycleList.js
@@ -0,0 +1,17 @@
+function computeListCycle(elements, resultLength)
+{
+    let listCycle = [];
+    for(let index = 0; index < resultLength; index++)
+    {
+        listCycle.push(elements[index % elements.length]);
+    }
+    return listCycle;
+}
+
+function main()
+{
+    var elements = [1, 2, 3, 4];
+    console.log("Cycle of elements is : "  + computeListCycle(elements, 9));
+}
+
+main();
