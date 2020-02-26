b2dc4b548ebb74b28a8b97c8abfeddeac7953a75
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-26 14:35:41 +0530

    removed main call

--- dropNElements.js
+++ dropNElements.js
@@ -8,5 +8,3 @@ function main()
     let elements = [1, 3, 9, 10, 11, 8], dropCount = 3;
     console.log("Elements after dropping " + dropCount + " elements is " + dropFirstNElements(elements, dropCount));
 }
-
-main();
