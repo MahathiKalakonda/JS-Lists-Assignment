492dd910fb9d06458e9af09bf01c565fe8e59674
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-26 17:51:11 +0530

    created rotateLeft file

---
+++ rotateLeft.js
@@ -0,0 +1,15 @@
+function rotateLeft(elements, rotateCount)
+{
+    rotatedElements = [];
+    for(let index = 0; index < elements.length; index++)
+    {
+        indexInElements = (index + rotateCount) % elements.length;
+        rotatedElements.push(elements[indexInElements]);
+    }
+    return rotatedElements;
+}
+
+function main()
+{
+    console.log("Elements after rotating are : " + rotateLeft([1, 2, 3, 4, 5], 2));
+}
