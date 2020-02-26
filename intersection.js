ea3253227749e50c0a4ce2b4e41bb7b98273f98f
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-26 15:15:26 +0530

    created intersection file

---
+++ intersection.js
@@ -0,0 +1,17 @@
+function intersection(firstBatch, secondBatch)
+{
+    let intersectingElements = [], differenceElements = firstBatch;
+    for(let index = 0; index < secondBatch.length; index++)
+    {
+        let indexInFirstBatch = differenceElements.indexOf(secondBatch[index])
+        if(indexInFirstBatch != -1)
+            intersectingElements.push(differenceElements.splice(indexInFirstBatch, 1));
+    }
+    return intersectingElements;
+}
+
+function main()
+{
+    let firstBatch = [1, 2, 3, 4], secondBatch = [1, 2, 2, 5];
+    console.log("Intersection is : " + intersection(firstBatch, secondBatch));
+}   
