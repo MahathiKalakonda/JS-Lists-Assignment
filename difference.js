bdd6167780b634d37faef520a8556b2190ae8e91
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-01 22:35:39 +0530

    used filter function

--- difference.js
+++ difference.js
@@ -1,18 +1,17 @@
 function difference(mainElements, removableElements)
 {
-    let differenceElements = mainElements;
-    for(let index = 0; index < removableElements.length; index++)
-    {
-        let indexInMainElements = differenceElements.indexOf(removableElements[index]);
-        if(indexInMainElements != -1)
-            differenceElements.splice(indexInMainElements, 1);
-    }
-    return differenceElements;
+    let elements = removableElements;
+    return differenceElements = mainElements.filter(function(element)
+                       {   indexInRemovable = elements.indexOf(element);
+                           delete elements[indexInRemovable];
+                           if(indexInRemovable == -1)
+                               return element;
+                       });
 }
 
 function main()
 {
-    mainElements = [1, 2, 3, 4, 2], removableElements = [1, 2, 3];
+    let mainElements = [1, 2, 3, 4, 2], removableElements = [1, 2, 3];
     console.log("Elements after  difference : " + difference(mainElements, removableElements));
 }
 
